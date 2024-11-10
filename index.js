d4cebf6ac2428c73483dbaeb80efda7e6ea20562
prasanthST <casinoprasanth@gmail.com>
2024-11-10 16:17:16 +0530

    resolved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import  express from  'express';
 import cors from  'cors';
 import dotenv from 'dotenv';
-import connectDB from './Database/dbconfig.js';
+import connectDB from './Database/dbConfig.js';
 import studRouter from './Router/Student.router.js';
 import mentorRouter from './Router/Mentor.router.js';
 
